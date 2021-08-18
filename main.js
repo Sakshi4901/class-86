08cd11fb30d99186140a2ebf834c5b1bca9659f4
Sakshi4901 <77976304+Sakshi4901@users.noreply.github.com>
2021-08-18 18:59:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const { fabric } = require("./fabric");
+
+var canvas=new fabric.canvas('myCanvas')
+block_img_width=30;
+block_img_height=30;
+player_x=10;
+player_y=10
+var player_object="";
+function player_update()
+{
+    fabric.Image.fromURL("player.png", function(Img) {
+        player_object= Img;
+        player_object.scaleToWidth(150);
+        player_object.scaleToHeight(140);
+        player_object.scaleToHeight({
+            top:player_y,
+            left:player_x
+        })
+        canvas.add()player_object;
+    }
+}
+ 
+function new_image(get_image)
+{
+    fabric.Image.fromURL(get_image, function(Img) {
+    block_image_object= Img;
+    block_image_object.scaleToWidth(block_image_width);
+    block_image_object.scaleToHeight(block_image_height);
+    block_image_object.set({
+            top:player_y,
+            left:player_x
+        })
+        canvas.add()block_image_object;
+}
